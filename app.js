bfd885d1770e3dc8d1c87d36ad635ad97466dbf4
Your Name <you@example.com>
2019-02-11 18:52:25 -0500

    updated Homework

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const patientList = [
   'Tri',
   'Maryam'
 ]
-
-
- const command = prompt('Choose one: update, delete, add, reorder');
-
+// 
 
 let add = function (){
+  // let command = prompt('Choose one: update, delete, add, reorder');
   alert(patientList);
   const addPatient = prompt("Enter Patient Name");
   patientList.push(addPatient);
@@ -24,6 +22,7 @@ let add = function (){
 };
 
 let update = function (){
+  // let command = prompt('Choose one: update, delete, add, reorder ');
   alert(patientList);
   let getPatient = prompt("Enter person from existing Patient List");
   console.log(getPatient);
@@ -35,16 +34,16 @@ let update = function (){
 };
 
 let deleted = function(){
+  // let command = prompt('Choose one: update, delete, add, reorder');
   alert(patientList);
   let getPatient = prompt("Enter person from existing Patient List");
   let indexOf = patientList.indexOf(getPatient);
-  // const deletePatient = prompt("Delete Patient List by index number");
-  // console.log(`Patient: ${patientList[deletePatient]} has been removed`);
   patientList.splice(indexOf, 1);
   alert(patientList);
 };
 
 let reorder = function (){
+  // let command = prompt('Choose one: update, delete, add, reorder');
   alert(patientList);
   const getPatient = prompt("Select Patient to be replaced ");
   let indexOf = patientList.indexOf(getPatient);
@@ -52,32 +51,29 @@ let reorder = function (){
   const replacePatient = prompt("Enter Patient Name")
   const indexOf2 = patientList.indexOf(replacePatient)
   console.log(indexOf2);
-  [patientList[indexOf],patientList[indexOf2]] = [patientList[indexOf2],patientList[indexOf]];
+  patientList.splice(indexOf, 1, replacePatient);
+  patientList.splice(indexOf2,1, getPatient);
+  // [patientList[indexOf],patientList[indexOf2]] = [patientList[indexOf2],patientList[indexOf]];
   alert(patientList);
 };
-
-
-
-while (command !== "add" && command !== "update" && command !== "delete" && command !== "reorder"){
-  alert("You entered the wrong command type")
-}
-if(command === "add"){
-      add();
-     // patientList[i] + addPatient;
-     console.log("add")
-   }
-if (command === "update"){
-     // patientList[i] + updatePatient;
-     update();
-     console.log("update")
-   }
-if (command === "delete"){
-     // patientList[i] - deletePatient;
-     deleted();
-     console.log("delete")
-   }
-if (command === "reorder"){
-     // patientList[i]
-     reorder();
-    console.log("reorder")
+let command = prompt('Choose one: update, delete, add, reorder');
+switch(command){
+  case "add":
+  // let command = prompt('Choose one: update, delete, add, reorder');
+  add();
+  break;
+  case "update":
+  // let command = prompt('Choose one: update, delete, add, reorder');
+  update();
+  break;
+  case "delete":
+  // let command = prompt('Choose one: update, delete, add, reorder');
+  deleted()
+  break;
+  case "reorder":
+  // let command = prompt('Choose one: update, delete, add, reorder');
+  reorder();
+  break;
+  default:
+  alert("Command not recognized...")
 }
